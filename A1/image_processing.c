b9800095660f4468564faba5cfb3e59225366a30
loledison199 <loledison199@gmail.com>
2020-01-15 20:39:09 -0500

    starter code

---
+++ A1/image_processing.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/*
+ * Read an image from the standard input and set the red value of each pixel to
+ * zero.
+ */
+void remove_red()
+{
+   
+}
+
+/*
+ * Read an image from the standard input and convert it from colour to black and white.
+ */
+void convert_to_black_and_white()
+{
+  
+}
+
+/*
+ * Read an image from the standard input and convert it to a square image.
+ */
+void instagram_square()
+{
+ 
+}
+
